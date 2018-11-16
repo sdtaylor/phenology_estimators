654d6d1b2e596d15d8df2ef44fa2d3c727deaf9e
Shawn Taylor <shawntaylor@weecology.org>
2018-11-16 11:54:18 -0500

    parse out data into the various subsets for analysis
    ie. different sample sizes of data and percent of yes/no

---
+++ config.R
@@ -0,0 +1,13 @@
+
+# Configuration for various analysis choices
+# In the initial_data_formatting script, these
+# values will be used to build the dataset used by
+# the estimators. 
+# For each year this will be x number of samples, consiting of x percent of observations flowering=1,
+# and repated x number of times (bootstraps) to get variation. 
+
+# In the estimator script, each estimator will be applied to each permutation.
+
+num_bootstraps = 10
+sample_sizes  = c(10,50,100)
+percent_yes   = c(0.25, 0.5, 0.75)
--- initial_data_formatting.R
+++ initial_data_formatting.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(lubridate)
 
+source('config.R')
+
 ################################################################
 # This script takes the raw data from Waananen et al. 2018 and converts
 # it to the format used in this analysis
@@ -58,5 +60,59 @@ all_possible_days = all_possible_days %>%
 doy_data = all_possible_days %>%
   left_join(doy_data, by=c('plant_id','doy','year')) %>%
   mutate(flowering = ifelse(is.na(flowering), 0, flowering))
+
+#####################################################################
+#####################################################################
+# Now setup random sampling for input into the estimators
+
+flowering_data_for_estimators = data.frame()
+
+for(this_year in unique(doy_data$year)){
+  for(this_sample_size in sample_sizes){
+    for(this_percent_yes in percent_yes){
+      for(bootstrap_i in 1:num_bootstraps){
+        
+        year_data = doy_data %>%
+          filter(year == this_year) %>%
+          select(year, doy, flowering)
+        
+        # Drop all observations past the very last flowering date for the year,
+        # since estimators are designed to work with data leading up to flowering.
+        last_flowering_doy = year_data %>%
+          filter(flowering==1) %>%
+          pull(doy) %>%
+          max()
+        year_data = year_data %>%
+          filter(doy <= last_flowering_doy)
+        
+        # Get random samples of flowering 'yes' and flowering 'no'
+        total_yes_obs = ceiling(this_sample_size * this_percent_yes)
+        total_no_obs  = this_sample_size - total_yes_obs
+        
+        flowering_yes = year_data %>%
+          filter(flowering == 1) %>%
+          sample_n(size = total_yes_obs, replace = FALSE)
+        flowering_no  = year_data %>%
+          filter(flowering == 0) %>%
+          sample_n(size = total_no_obs, replace = FALSE)
+        
+        # Label it with global parameters and package into the
+        # the final data.frame.
+        both = flowering_yes %>%
+          bind_rows(flowering_no) %>%
+          mutate(sample_size = this_sample_size,
+                 percent_yes = this_percent_yes,
+                 bootstrap_i = bootstrap_i)
+        
+        flowering_data_for_estimators = flowering_data_for_estimators %>%
+          bind_rows(both)
+        
+        
+      }
+    }
+  }
+}
+
+write_csv(flowering_data_for_estimators, 'data/flowering_data_for_estimators.R')
 
 
